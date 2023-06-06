a2f24c6a65887a74c7e385e63b4902ce46b6d086
zhmei21 <55950782+zhmei21@users.noreply.github.com>
2023-06-06 11:07:30 +0200

    Add files via upload

---
+++ pythonWriteAndSafe.py
@@ -0,0 +1,25 @@
+print("Writing and reading a file with the name: example.txt")
+try:
+    with open("example.txt", 'w') as file:
+        userInput = input("Write something: ")
+        file.write(userInput)
+        file.write("\nEnd")
+
+except Exception as e:
+    print("An error has occured:", str(e))
+
+try:
+    with open("example.txt", 'r') as file:
+        for line_num, content in enumerate(file, start=1):
+            print("Line %d: %s" % (line_num, content))
+
+except Exception as e:
+    print("An error has occurred while reading the file:", str(e))
+
+f = open("example.txt", "r")
+
+#lines = 1
+#for content in f:
+#    print("Line %d: %s"%(lines, content))
+#    lines+= 1
+#f.close()
